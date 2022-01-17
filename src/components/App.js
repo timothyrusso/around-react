b7ab15ba9f0a2c1cd5e48854b9515e3894e2851c
timothyrusso <russotimothy@live.it>
2022-01-17 17:43:51 +0100

    fix: code refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
+import React from 'react';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 
 function App() {
+
+  const handleEditAvatarClick = () => {
+
+  }
+
+  React.useEffect(() => {
+    document.body.classList.add('page');
+
+    return () => {
+      document.body.classList.remove('page');
+    };
+  })
+
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <div className="content">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
     </>
   );
 }
---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PopupWithForm() {
+    return (
+        <header className="header">
+            <img src={logo} alt="Logo representing the Around the US project" className="logo" />
+        </header>
+    )
+}
+
+export default PopupWithForm;

914c3ae84fb851bc1577ff04337d9da3c847092f
timothyrusso <russotimothy@live.it>
2022-01-17 16:54:48 +0100

    feat: creation of the main modules

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <p className="footer__copyright">© 2022 Timothy Russo - Around The U.S.</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Main.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Main() {
+    return (
+        <main className="main">
+            <section className="profile">
+                <div className="profile__image-wrapper">
+                    <img src="#" alt="Avatar of the page profile" className="profile__image" />
+                        <button aria-label="Edit" type="button" className="profile-image-button"></button>
+                </div>
+                <div className="profile__wrapper">
+                    <h1 className="profile__name"></h1>
+                    <button aria-label="Edit" type="button" className="edit-button"></button>
+                    <p className="profile__about-me"></p>
+                </div>
+                <button aria-label="Add" type="button" className="add-button"></button>
+            </section>
+
+            <section className="gallery">
+                <ul className="cards-grid"></ul>
+            </section>
+        </main>
+    )
+}
+
+export default Main;

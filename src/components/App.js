b4c5d1887a7ae1a01b9f0e863cc2ccc62490e3ce
timothyrusso <russotimothy@live.it>
2022-01-20 14:30:52 +0100

    fix: code refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
@@ -11,7 +11,7 @@ function App() {
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false)
   const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = useState(false)
   const [isConfirmationPopupOpen, setIsConfirmationPopupOpen] = useState(false)
-  const [selectedCard, setIsselectedCard] = useState(undefined)
+  const [selectedCard, setSelectedCard] = useState(undefined)
 
   function handleEditAvatarClick() {
     setIsEditAvatarPopupOpen(true)
@@ -34,53 +34,43 @@ function App() {
     setIsEditProfilePopupOpen(false)
     setIsAddPlacePopupOpen(false)
     setIsConfirmationPopupOpen(false)
-    setIsselectedCard(undefined)
+    setSelectedCard(undefined)
   }
 
   function handleCardClick(card) {
-    setIsselectedCard(card)
+    setSelectedCard(card)
   }
 
-  useEffect(() => {
-    document.body.classList.add("page");
-
-    return () => {
-      document.body.classList.remove("page");
-    };
-  })
-
   return (
-    <>
-      <div className="content">
-        <Header />
-        <Main onEditAvatarClick={handleEditAvatarClick} onEditProfileClick={handleEditProfileClick} onAddPlaceClick={handleAddPlaceClick} onCardClick={handleCardClick} onDeleteClick={handleConfirmationClick}>
-          <PopupWithForm name="edit" title="Edit profile" isOpen={isEditProfilePopupOpen} onClose={closeAllPopups} buttonText={"Save"}>
-            <input type="text" id="name-input" name="name" className="popup__input popup__input_field_name" placeholder="Name" required
-              minLength="2" maxLength="40" />
-            <span id="name-input-error"></span>
-            <input type="text" id="aboutMe-input" name="aboutMe" className="popup__input popup__input_field_about-me" placeholder="Description" required
-              minLength="2" maxLength="200" />
-            <span id="aboutMe-input-error"></span>
-          </PopupWithForm>
-          <PopupWithForm name="add" title="New place" isOpen={isAddPlacePopupOpen} onClose={closeAllPopups} buttonText={"Create"}>
-            <input type="text" id="title-input" name="title" className="popup__input popup__input_field_title"
-              placeholder="Title" required minLength="1" maxLength="30" />
-            <span id="title-input-error"></span>
-            <input type="url" id="link-input" name="link" className="popup__input popup__input_field_link"
-              placeholder="Image link" required />
-            <span id="link-input-error"></span>
-          </PopupWithForm>
-          <PopupWithForm name="profile-image" title="Change profile picture" isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups} buttonText={"Create"}>
-            <input type="url" id="image-link-input" name="link" className="popup__input popup__input_image_link"
-              placeholder="Image link" required />
-            <span id="image-link-input-error"></span>
-          </PopupWithForm>
-          <PopupWithForm name="delete-card" title="Are you sure?" confirmationButtonClass={"submit-button_type_delete-card"} confirmationTitleClass={"popup__title_type_delete-card"} isOpen={isConfirmationPopupOpen} onClose={closeAllPopups} buttonText={"Yes"} />
-          <ImagePopup card={selectedCard} onClose={closeAllPopups} />
-        </Main>
-        <Footer />
-      </div>
-    </>
+    <div className="content">
+      <Header />
+      <Main onEditAvatarClick={handleEditAvatarClick} onEditProfileClick={handleEditProfileClick} onAddPlaceClick={handleAddPlaceClick} onCardClick={handleCardClick} onDeleteClick={handleConfirmationClick}>
+        <PopupWithForm name="edit" title="Edit profile" isOpen={isEditProfilePopupOpen} onClose={closeAllPopups} buttonText={"Save"}>
+          <input type="text" id="name-input" name="name" className="popup__input popup__input_field_name" placeholder="Name" required
+            minLength="2" maxLength="40" />
+          <span id="name-input-error"></span>
+          <input type="text" id="aboutMe-input" name="aboutMe" className="popup__input popup__input_field_about-me" placeholder="Description" required
+            minLength="2" maxLength="200" />
+          <span id="aboutMe-input-error"></span>
+        </PopupWithForm>
+        <PopupWithForm name="add" title="New place" isOpen={isAddPlacePopupOpen} onClose={closeAllPopups} buttonText={"Create"}>
+          <input type="text" id="title-input" name="title" className="popup__input popup__input_field_title"
+            placeholder="Title" required minLength="1" maxLength="30" />
+          <span id="title-input-error"></span>
+          <input type="url" id="link-input" name="link" className="popup__input popup__input_field_link"
+            placeholder="Image link" required />
+          <span id="link-input-error"></span>
+        </PopupWithForm>
+        <PopupWithForm name="profile-image" title="Change profile picture" isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups} buttonText={"Create"}>
+          <input type="url" id="image-link-input" name="link" className="popup__input popup__input_image_link"
+            placeholder="Image link" required />
+          <span id="image-link-input-error"></span>
+        </PopupWithForm>
+        <PopupWithForm name="delete-card" title="Are you sure?" confirmationButtonClass={"submit-button_type_delete-card"} confirmationTitleClass={"popup__title_type_delete-card"} isOpen={isConfirmationPopupOpen} onClose={closeAllPopups} buttonText={"Yes"} />
+        <ImagePopup card={selectedCard} onClose={closeAllPopups} />
+      </Main>
+      <Footer />
+    </div>
   );
 }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -7,14 +7,14 @@ function Card({ card, onCardClick, onDeleteClick }) {
     }
 
     return (
-        <li className="card" key={card._id}>
+        <li className="card">
             <button aria-label="Delete" type="button" className="card__delete" onClick={onDeleteClick}></button>
-            <div className="card__image" style={{ backgroundImage: `url(${card.link})` }} alt={card.name} onClick={handleClick}></div>
+            <div className="card__image" style={{ backgroundImage: `url(${card.link})` }} onClick={handleClick}></div>
             <div className="card__content">
                 <h2 className="card__title">{card.name}</h2>
                 <div className="card__like-wrapper">
                     <button aria-label="Like" type="button" className="card__like"></button>
-                    <p className="card__like-counter"></p>
+                    <p className="card__like-counter">{card.likes.length}</p>
                 </div>
             </div>
         </li>

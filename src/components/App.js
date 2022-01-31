1fddb4841ae3757470929ebaf5e5940e8da53b26
timothyrusso <russotimothy@live.it>
2022-01-31 20:39:56 +0100

    fix: code refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import Footer from "./Footer";
 import PopupWithForm from "./PopupWithForm";
 import ImagePopup from "./ImagePopup";
 import api from "../utils/api";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
+import { CardContext } from "../contexts/CardContext";
 
 function App() {
 
@@ -14,6 +16,7 @@ function App() {
   const [isConfirmationPopupOpen, setIsConfirmationPopupOpen] = useState(false)
   const [selectedCard, setSelectedCard] = useState(undefined)
   const [currentUser, setCurrentUser] = useState("")
+  const [cards, setCards] = useState("")
 
   function handleEditAvatarClick() {
     setIsEditAvatarPopupOpen(true)
@@ -45,44 +48,58 @@ function App() {
 
   React.useEffect(() => {
     api.getProfileInfo()
-        .then((info) => {
-          setCurrentUser(info._id)
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-}, [])
+      .then((info) => {
+        setCurrentUser(info)
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+
+    api.getCards()
+      .then((data) => {
+        setCards(data)
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }, [])
+
+  console.log(cards)
 
   return (
-    <div className="content">
-      <Header />
-      <Main onEditAvatarClick={handleEditAvatarClick} onEditProfileClick={handleEditProfileClick} onAddPlaceClick={handleAddPlaceClick} onCardClick={handleCardClick} onDeleteClick={handleConfirmationClick}>
-        <PopupWithForm name="edit" title="Edit profile" isOpen={isEditProfilePopupOpen} onClose={closeAllPopups} buttonText={"Save"}>
-          <input type="text" id="name-input" name="name" className="popup__input popup__input_field_name" placeholder="Name" required
-            minLength="2" maxLength="40" />
-          <span id="name-input-error"></span>
-          <input type="text" id="aboutMe-input" name="aboutMe" className="popup__input popup__input_field_about-me" placeholder="Description" required
-            minLength="2" maxLength="200" />
-          <span id="aboutMe-input-error"></span>
-        </PopupWithForm>
-        <PopupWithForm name="add" title="New place" isOpen={isAddPlacePopupOpen} onClose={closeAllPopups} buttonText={"Create"}>
-          <input type="text" id="title-input" name="title" className="popup__input popup__input_field_title"
-            placeholder="Title" required minLength="1" maxLength="30" />
-          <span id="title-input-error"></span>
-          <input type="url" id="link-input" name="link" className="popup__input popup__input_field_link"
-            placeholder="Image link" required />
-          <span id="link-input-error"></span>
-        </PopupWithForm>
-        <PopupWithForm name="profile-image" title="Change profile picture" isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups} buttonText={"Create"}>
-          <input type="url" id="image-link-input" name="link" className="popup__input popup__input_image_link"
-            placeholder="Image link" required />
-          <span id="image-link-input-error"></span>
-        </PopupWithForm>
-        <PopupWithForm name="delete-card" title="Are you sure?" confirmationButtonClass={"submit-button_type_delete-card"} confirmationTitleClass={"popup__title_type_delete-card"} isOpen={isConfirmationPopupOpen} onClose={closeAllPopups} buttonText={"Yes"} />
-        <ImagePopup card={selectedCard} onClose={closeAllPopups} />
-      </Main>
-      <Footer />
-    </div>
+    <CurrentUserContext.Provider value={currentUser}>
+      <CardContext.Provider value={cards}>
+        <div className="content">
+          <Header />
+          <Main onEditAvatarClick={handleEditAvatarClick} onEditProfileClick={handleEditProfileClick} onAddPlaceClick={handleAddPlaceClick} onCardClick={handleCardClick} onDeleteClick={handleConfirmationClick}>
+            <PopupWithForm name="edit" title="Edit profile" isOpen={isEditProfilePopupOpen} onClose={closeAllPopups} buttonText={"Save"}>
+              <input type="text" id="name-input" name="name" className="popup__input popup__input_field_name" placeholder="Name" required
+                minLength="2" maxLength="40" />
+              <span id="name-input-error"></span>
+              <input type="text" id="aboutMe-input" name="aboutMe" className="popup__input popup__input_field_about-me" placeholder="Description" required
+                minLength="2" maxLength="200" />
+              <span id="aboutMe-input-error"></span>
+            </PopupWithForm>
+            <PopupWithForm name="add" title="New place" isOpen={isAddPlacePopupOpen} onClose={closeAllPopups} buttonText={"Create"}>
+              <input type="text" id="title-input" name="title" className="popup__input popup__input_field_title"
+                placeholder="Title" required minLength="1" maxLength="30" />
+              <span id="title-input-error"></span>
+              <input type="url" id="link-input" name="link" className="popup__input popup__input_field_link"
+                placeholder="Image link" required />
+              <span id="link-input-error"></span>
+            </PopupWithForm>
+            <PopupWithForm name="profile-image" title="Change profile picture" isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups} buttonText={"Create"}>
+              <input type="url" id="image-link-input" name="link" className="popup__input popup__input_image_link"
+                placeholder="Image link" required />
+              <span id="image-link-input-error"></span>
+            </PopupWithForm>
+            <PopupWithForm name="delete-card" title="Are you sure?" confirmationButtonClass={"submit-button_type_delete-card"} confirmationTitleClass={"popup__title_type_delete-card"} isOpen={isConfirmationPopupOpen} onClose={closeAllPopups} buttonText={"Yes"} />
+            <ImagePopup card={selectedCard} onClose={closeAllPopups} />
+          </Main>
+          <Footer />
+        </div>
+      </CardContext.Provider>
+    </CurrentUserContext.Provider>
   );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,45 +1,24 @@
 import React from "react";
-import api from "../utils/api";
 import Card from "./Card";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
+import { CardContext } from "../contexts/CardContext";
 
 function Main({ onEditAvatarClick, onEditProfileClick, onAddPlaceClick, onCardClick, onDeleteClick, children }) {
 
-    const [userName, setUserName] = React.useState("");
-    const [userDescription, setUserDescription] = React.useState("");
-    const [userAvatar, setUserAvatar] = React.useState("");
-    const [cards, setCards] = React.useState([])
-
-    React.useEffect(() => {
-        api.getProfileInfo()
-            .then((info) => {
-                setUserName(info.name)
-                setUserDescription(info.about)
-                setUserAvatar(info.avatar)
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-
-        api.getCards()
-            .then((data) => {
-                setCards(data)
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-    }, [])
+    const currentUser = React.useContext(CurrentUserContext);
+    const cards = React.useContext(CardContext)
 
     return (
         <main className="main">
             <section className="profile">
                 <div className="profile__image-wrapper" onClick={onEditAvatarClick}>
-                    <img src={userAvatar} alt="Avatar of the page profile" className="profile__image" />
+                    <img src={currentUser.avatar} alt="Avatar of the page profile" className="profile__image" />
                     <button aria-label="Edit" type="button" className="profile-image-button"></button>
                 </div>
                 <div className="profile__wrapper">
-                    <h1 className="profile__name">{userName}</h1>
+                    <h1 className="profile__name">{currentUser.name}</h1>
                     <button aria-label="Edit" type="button" className="edit-button" onClick={onEditProfileClick}></button>
-                    <p className="profile__about-me">{userDescription}</p>
+                    <p className="profile__about-me">{currentUser.about}</p>
                 </div>
                 <button aria-label="Add" type="button" className="add-button" onClick={onAddPlaceClick}></button>
             </section>

d2370ce502d11e19676d4473e91b6df76672abf0
Timothy Russo <russo.t@cisalfasport.it>
2022-02-11 15:08:12 +0100

    fix: input refactoring

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -1,23 +1,23 @@
-// import { useRef } from "react";
+import { useRef } from "react";
 import PopupWithForm from "./PopupWithForm";
-import ImageInput from "./ImageInput";
 
 function EditAvatarPopup({ isOpen, onClose, onUpdateAvatar, isLoading, startLoading }) {
 
-  // const avatarRef = useRef()
+  const avatarRef = useRef()
 
   function handleSubmit(evt) {
     startLoading()
     evt.preventDefault()
 
-    // onUpdateAvatar({
-    //   avatar: avatarRef.current.value
-    // });
+    onUpdateAvatar({
+      avatar: avatarRef.current.value
+    });
   }
 
   return (
     <PopupWithForm name="profile-image" title="Change profile picture" isOpen={isOpen} onClose={onClose} buttonText={"Create"} loadingText={"Saving.."} isLoading={isLoading} onSubmit={handleSubmit}>
-      <ImageInput type={"url"} idName={"image-link-input"} name={"link"} fieldName={"image_link"} placeholder={"Image link"} onUpdate={onUpdateAvatar} />
+      <input type="url" id="image-link-input" name="link" className="popup__input popup__input_image_link" placeholder="Image link" ref={avatarRef} required />
+      <span id="image-link-input"></span>
     </PopupWithForm>
   )
 }
